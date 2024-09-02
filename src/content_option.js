167116f783cbb0403e6f2f8c277f7928846da5dc
rohinishraj124 <rohinishraj124@gmail.com>
2024-09-02 20:28:42 +0530

    Initial commit

---
+++ src/content_option.js
@@ -0,0 +1,110 @@
+const logotext = "RR";
+const meta = {
+    title: "Rohinish Raj",
+    description: "I’m Rohinish Raj Web Developer _ Full stack devloper,currently study in IIT BBS",
+};
+
+const introdata = {
+    title: "I’m Rohinish Raj",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I wanna be a web developer",
+    },
+    description: "I’m Rohinish Raj, an Electronics and Communication Engineering student at IIT Bhubaneswar with a passion for full-stack development. Explore my projects and achievements as I pursue my goal of becoming a versatile developer.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "My name is Rohinish Raj from Siwan, Bihar. Currently, I am a 1st-year student at Indian Institute of Technology, Bhubaneshwar. I have done my 10th and 12th from Mahaviri Saraswati Vidya Mandir. Right now, I'm learning how to build websites using HTML, CSS, and JavaScript. I find it super cool how you can create awesome stuff on the internet with these tools. My hobbies are playing Basketball, sleeping, and watching movies. My goal is to learn more about making different parts of websites and eventually become really good at it. I'm excited about becoming a skilled web developer and working on all sorts of interesting web projects.",
+};
+const worktimeline = [{
+    jobtitle: "10th",
+    where: "Siwan",
+    date: "2020 - 2021",
+},
+{
+    jobtitle: "12th",
+    where: "Siwan",
+    date: "2021 - 2023",
+},
+{
+    jobtitle: "BTech",
+    where: "IIT Bhubaneshwar",
+    date: "2023 - continue",
+},
+];
+
+const skills = [{
+    name: "C",
+    value: 80,
+},
+{
+    name: "HTML",
+    value: 90,
+},
+{
+    name: "CSS",
+    value: 80,
+},
+{
+    name: "JavaScript",
+    value: 70,
+},
+{
+    name: "React",
+    value: 80,
+},
+{
+    name: "CPP",
+    value: 70,
+},
+{
+    name: "Python",
+    value: 60,
+},
+];
+
+const services = [{
+    title: "Society Of IIT BBS",
+    description: "Currently , I am a active member of WebnD (Website Developent Society) and Nakshatra(Astronaumy Society)",
+},
+];
+
+const dataportfolio = [{
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgSVMLJAybwkPi2a8EjrNSjQySErCvnOH1Kg&s",
+    description: "Stay ahead of the storm with real-time weather updates.",
+    link: "https://rohinishraj124.github.io/Weather/",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "rohinishraj124@gmail.com",
+    YOUR_FONE: "8521671402",
+    description: "I’m excited to hear from you! Whether you’re interested in discussing a project, have questions about my work, or just want to chat, don’t hesitate to reach out. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_yiggfdj",
+    YOUR_TEMPLATE_ID: "template_3fj8ygc",
+    YOUR_USER_ID: "rohinishraj",
+};
+
+const socialprofils = {
+    github: "https://github.com/rohinishraj124",
+    instagram: "https://www.instagram.com/_raj_rohinish_",
+    linkedin: "https://www.linkedin.com/in/rohinish-raj-734551297/",
+    twitter: "https://x.com/RajRohinish1204",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
